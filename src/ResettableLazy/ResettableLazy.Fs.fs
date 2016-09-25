87219d8510194e7da1669f23dc1438590e3a46ef
Jono Job <fruitraccoon@outlook.com>
2016-09-25 16:52:53 +1000

    Add option for using an async valueFactory

--- src/ResettableLazy/ResettableLazy.Fs.fs
+++ src/ResettableLazy/ResettableLazy.Fs.fs
@@ -3,13 +3,26 @@
 type private Query<'a> =
     | Reset
     | IsCreated of AsyncReplyChannel<bool>
-    | Get of AsyncReplyChannel<'a>
+    | Get of AsyncReplyChannel<Async<'a>>
 
 type ResettableLazy<'a> = private ResettableLazy of MailboxProcessor<Query<'a>>
 
 module ResettableLazy =
 
-    let create valueFactory =
+    /// Creates an asynchronous workflow that runs the asynchronous workflow given as an argument at most once.
+    /// When the returned workflow is started for the second time, it reuses the result of the previous execution.
+    /// From: https://github.com/fsprojects/FSharpx.Async/blob/b3a8c41813f04862a9ea1935a051c5ed65fa20d4/src/FSharpx.Async/Async.fs#L67
+    let private asyncCache (input:Async<'T>) =
+        let agent = MailboxProcessor<AsyncReplyChannel<_>>.Start(fun agent -> async {
+            let! repl = agent.Receive()
+            let! res = input
+            repl.Reply(res)
+            while true do
+                let! repl = agent.Receive()
+                repl.Reply(res) })
+        agent.PostAndAsyncReply(id)
+
+    let createAsync valueFactory =
         let applyQuery value query =
             match query with
             | Reset ->
@@ -20,7 +33,7 @@ module ResettableLazy =
             | Get rc ->
                 let v' = match value with
                          | Some v -> v
-                         | None -> valueFactory()
+                         | None -> valueFactory() |> asyncCache
                 do rc.Reply(v')
                 Some v'
         let agent = MailboxProcessor<Query<'a>>.Start(fun inbox ->
@@ -31,11 +44,14 @@ module ResettableLazy =
             messageLoop None)
         ResettableLazy agent
 
+    let create valueFactory =
+        createAsync (valueFactory >> async.Return)
+
     let isValueCreated (ResettableLazy agent) =
         agent.PostAndAsyncReply IsCreated
 
     let value (ResettableLazy agent) =
-        agent.PostAndAsyncReply Get
+        async.Bind(agent.PostAndAsyncReply Get, id)
 
     let reset (ResettableLazy agent) =
         agent.Post Reset
