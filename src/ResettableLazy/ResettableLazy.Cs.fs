7b1cd5f9a5d0e055ecbed78dc65c6b5352de33d4
Jono Job <fruitraccoon@outlook.com>
2016-09-25 15:35:58 +1000

    Add wrapper type for CSharp usage

---
+++ src/ResettableLazy/ResettableLazy.Cs.fs
@@ -0,0 +1,16 @@
+﻿namespace ResettableLazy.Cs
+
+open ResettableLazy
+
+type ResettableLazy<'T>(valueFactory) =
+
+    let rl = Fs.ResettableLazy.create valueFactory
+
+    member x.IsValueCreated =
+        Fs.ResettableLazy.isValueCreated rl |> Async.StartAsTask
+
+    member x.Value =
+        Fs.ResettableLazy.value rl |> Async.StartAsTask
+
+    member x.Reset () =
+        Fs.ResettableLazy.reset rl
