985177b44286de8188f566fe8ad357e3ea8e3c04
Jono Job <fruitraccoon@outlook.com>
2016-09-25 10:47:11 +1000

    Add first test

---
+++ src/ResettableLazy.Tests/ResettableLazySpec.fs
@@ -0,0 +1,17 @@
+﻿module ResettableLazySpec
+
+open Xunit
+open FsUnit
+open FsUnit.Xunit
+open ResettableLazy.Fs
+
+module ``The value function`` =
+
+    let testResult = 42
+    let factory = fun () -> testResult
+
+    [<Fact>]
+    let ``Should return the factory result`` () =
+        let rl = ResettableLazy.create factory
+        let result = ResettableLazy.value rl |> Async.RunSynchronously
+        result |> should equal testResult
